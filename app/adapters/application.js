0c4c656736beb2f5b997608e839b9f94bff8a4c1
Amos Endersen <amenders@iupui.edu>
2016-12-31 22:56:26 -0500

    activated mirage

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-	host: 'https://retina-api-develop.azurewebsites.net',
-	namespace: 'api'
+//	host: 'https://retina-api-develop.azurewebsites.net',
+//	namespace: 'api'
 });

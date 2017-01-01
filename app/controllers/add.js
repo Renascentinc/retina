0c96582f332e19612a1100cfb646b44a1d2004b1
Josiah Campbell <jbcampbe@iupui.edu>
2017-01-01 03:20:37 -0500

    adding untracked files

---
+++ app/controllers/add.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        saveNewTool(tool) {
+            
+        },
+        
+        changed(input) {
+            console.log(input);
+        }
+    }
+});

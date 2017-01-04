4008ac6b76662544e1ae231f226ae4f4f62d41a9
Amos Endersen <amenders@iupui.edu>
2017-01-04 17:29:48 -0500

    worked on info page status update

--- app/routes/info.js
+++ app/routes/info.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(params) {
-		return this.get('store').findRecord('tool', params.id);
+		return Ember.RSVP.hash({ 
+		
+			tools: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/search?parameter=3'),
+			brands: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/brands')
+		
+		});
 	}
 });

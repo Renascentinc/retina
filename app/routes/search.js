abb47d687212375893241e4eec26ef9ee95dd348
Josiah Campbell <jbcampbe@iupui.edu>
2017-01-03 20:06:12 -0500

    pointed tools calls at the new search

--- app/routes/search.js
+++ app/routes/search.js
@@ -4,72 +4,7 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model() {
 		return Ember.RSVP.hash({
-			tools: { 
-                data: [{
-                type: 'tool',
-                id: '1',
-                attributes: {
-                  brand: 'Bosch',
-                  username: 'George Boole',
-                  type: 'hammer drill',
-                  status: 'in use'
-                }
-              }, {
-                type: 'tool',
-                id: '2',
-                attributes: {
-                  brand: 'DeWalt',
-                  username: 'Mike Flag',
-                  type: 'impact driver',
-                  status: 'in use'
-                }
-              }, {
-                type: 'tool',
-                id: '3',
-                attributes: {
-                  brand: 'Milwaukee',
-                  username: 'Sam Blam',
-                  type: 'reciprocating saw',
-                  status: 'available'
-                }
-              }, {
-                type: 'tool',
-                id: '3',
-                attributes: {
-                  brand: 'Milwaukee',
-                  username: 'Dr. Watt',
-                  type: 'reciprocating saw',
-                  status: 'available'
-                }
-              }, {
-                type: 'tool',
-                id: '234',
-                attributes: {
-                  brand: 'Milwaukee',
-                  username: 'Charlie Delta',
-                  type: 'reciprocating saw',
-                  status: 'available'
-                }
-              }, {
-                type: 'tool',
-                id: '10',
-                attributes: {
-                  brand: 'Milwaukee',
-                  username: 'Charlie Bravo',
-                  type: 'reciprocating saw',
-                  status: 'available'
-                }
-              }, {
-                type: 'tool',
-                id: '5',
-                attributes: {
-                  brand: 'Milwaukee',
-                  username: 'Bob Ross',
-                  type: 'reciprocating saw',
-                  status: 'available'
-                }
-              }]
-            },
+            tools: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/search?parameter=3'),
 			status: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/status'),
 			users: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/users'),
 			types: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/types'),

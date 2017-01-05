8593edbd360e7bb167054b1c9b05e9cfd870da32
ekraihan <ekraihan@iupui.edu>
2017-01-04 20:33:37 -0500

    finishing transfer

--- app/controllers/scan.js
+++ app/controllers/scan.js
@@ -7,38 +7,35 @@ export default Ember.Controller.extend({
             let toolid = Ember.$("#toolid").val();
             this.get('target').transitionTo('info', toolid);
         },
-		
+
 		goToTransferPage() {
 			let toolid = Ember.$("#toolid").val();
 			this.get('target').transitionTo('transfer', toolid);
 		},
-		
+
 		addToList(toolid){
             if( !this.toolList.includes(toolid) ) {
                 this.toolList.push(toolid);
                 Ember.$("#list").append(toolid + "<br>");
             }
 		},
-		
+
 		deleteToolList(){
 			this.toolList.splice(0, this.toolList.length);
 			$("#list").html("");
 		},
-        
+
         transferTools() {
-            let userid = 0;
-            const options = {
-                processData: false,
-                url: 'https://retina-app-develop.azurewebsites.net/api/transfer',
-                data: { userid: userid, toolids: this.toolList },
-                method: 'PUT',
-                dataType: 'json',
-                accept: 'application/json',
-                headers: {
-                "Content-Type": 'application/json'
-                }
-            };
-            
+           var userid = parseInt($("#user-to-transfer-to").val());
+
+           const options = {
+
+               url: 'https://retina-api-develop.azurewebsites.net/api/transfer',
+               data: { userid: userid, toolids: this.toolList},
+               type: 'PUT',
+               crossDomain: true
+           };
+
             Ember.$.ajax(options);
             Ember.$("#list").html("");
             this.toolList = [];

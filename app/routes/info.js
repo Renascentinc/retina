992ff6cfe24323fe343aec33a8b1521f35aef004
Josiah Campbell <jbcampbe@iupui.edu>
2017-01-07 04:18:56 -0500

    improved info page

--- app/routes/info.js
+++ app/routes/info.js
@@ -3,10 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model(params) {
 		return Ember.RSVP.hash({ 
-			tool: this.get('store').createRecord('tool'),
-			tools: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/tools/'+params.id),
+            tool: this.get('store').findRecord('tool', params.id),
 			status: Ember.$.getJSON('https://retina-api-develop.azurewebsites.net/api/status')
-		
 		});
 	}
 });

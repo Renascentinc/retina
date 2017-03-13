6fab004d82b5a974fa9426e5eca8f6b37ec791c5
Josiah Campbell <jbcampbe@iupui.edu>
2017-03-13 13:55:35 -0400

    validation has been added. Just need to find an alphanumeric regex

--- app/components/tool-form.js
+++ app/components/tool-form.js
@@ -1,17 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  showErrorMessage: false,
   actions: {
     saveNewTool(tool) {
         let _this = this;
 
-        return tool.save().then(function (tool) {
-          let toolid = tool.get('id');
-          _this.get('target').transitionTo('info', toolid);
+        return new Promise(function(resolve, reject) {
+          tool.validate().then(({validations}) => {
+            if (validations.get('isValid')) {
+              _this.set('showErrorMessage', false);
 
-        }).catch(function (e) {
-          Ember.Logger.info(e);
+              tool.save().then(function (tool) {
+                resolve('save successful');
+                let toolid = tool.get('id');
+                _this.get('target').transitionTo('info', toolid);
+
+              }).catch(function (e) {
+                reject('save failed');
+                Ember.Logger.info(e);
+              });
+
+            } else {
+              _this.set('showErrorMessage', true);
+              reject('validation failed');
+            }
+          });
         });
     },
 

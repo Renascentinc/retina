30c952ca5b26a0603e837e4e7cfaba882fe87552
Josiah Campbell <jbcampbe@iupui.edu>
2017-05-07 23:08:22 -0400

    solved some styling probs

--- app/routes/search.js
+++ app/routes/search.js
@@ -4,8 +4,8 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model() {
 		return Ember.RSVP.hash({
-      		tool: this.get('store').query('tool', {status: '', userID: '', type: '', brand: ''}),
-      		dropdown: this.get('store').queryRecord('dropdown', {currentUser: 0, brand: true, type: true, provider: false, status: true, user: true, restricteduser: false})
+			tool: this.get('store').query('tool', {status: '', userID: '', type: '', brand: ''}),
+			dropdown: this.get('store').queryRecord('dropdown', {currentUser: 0, brand: true, type: true, provider: false, status: true, user: true, restricteduser: false})
 		});
 	}
 });

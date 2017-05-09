334a758521bdd50ab5a851db5abcf068b98e5173
Amos Endersen <amenders@iupui.edu>
2017-05-08 23:04:28 -0400

    error messages reset when user edits password or username

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,10 +6,14 @@ export default Ember.Controller.extend({
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
-        
+
       this.get('session').authenticate('authenticator:oauth2', identification, password).catch(() => {
         this.set('errorMessage', 'Invalid Username/Password');
       });
+    },
+
+    clearError(){
+      this.set('errorMessage', "");
     }
   }
 });
--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,39 +1,39 @@
 import Ember from 'ember';
 import config from '../config/environment';
 
-export default Ember.Controller.extend({	
+export default Ember.Controller.extend({
     queryParams: {
         status: "",
         brand: "",
         type: "",
         userID: ""
     },
-    
+
     actions: {
         updateSearch( target ) {
-			Ember.$(".search-box").val('');
+			      Ember.$(".search-box").val('');
             let params = this.queryParams.get('0');
-            
+
             if( target.getAttribute('name') === "status" ) {
                 params.status = target.value;
-                
+
             } else if( target.getAttribute('name') === "brand" ) {
                 params.brand = target.value;
-                
+
             } else if( target.getAttribute('name') === "type" ) {
                 params.type = target.value;
-                
+
             } else if( target.getAttribute('name') === "owner" ) {
                 params.userID = target.value;
             }
-            
+
             var set = this.set.bind(this, 'model.tools');
             Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search', params ).then(set);
         },
-        
+
         fuzzySearch(value) {
             var set = this.set.bind(this, 'model.tools');
-            
+
             if( value !== "" ) {
 				Ember.$(".search-parameter").val('');
                 Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search', { parameter: value } ).then(set);
@@ -41,7 +41,7 @@ export default Ember.Controller.extend({
                 Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search?status=&userID=&type=&brand=').then(set);
             }
         },
-		
+
 		currentUser(){
 			return this.get('session').get('data.currentUserID');
 		}

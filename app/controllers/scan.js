eb1391f0d97b304157ca3b087881e157ffbad2ab
Amos Endersen <amenders@iupui.edu>
2017-05-12 23:39:25 -0400

    Amos.info page ui update (#88)
    * stash popping
    
    * fixed info page margin issue

--- app/controllers/scan.js
+++ app/controllers/scan.js
@@ -3,9 +3,9 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
     session: Ember.inject.service('session'),
-    
+
     toolList: [],
-    
+
     queryParams: {
         currentUser: 0,
         status: "",
@@ -17,7 +17,7 @@ export default Ember.Controller.extend({
     init() {
         this.get('queryParams').set('0.currentUser', this.get('session').get('data.currentUserID'));
     },
-    
+
     actions: {
         goToInfoPage() {
             let toolid = Ember.$("#toolid").val();
@@ -42,17 +42,17 @@ export default Ember.Controller.extend({
         transferTools() {
 			Ember.$("#user-to-transfer-to").css("border-style", "none");
 			Ember.$("#list-title").css("border-style", "none");
-			
+
 			if (Ember.$("#user-to-transfer-to").val() === null){
 				Ember.$("#user-to-transfer-to").css("border-style", "solid");
 				Ember.$("#user-to-transfer-to").css("border-color", "#e30000");
 			}
-			
+
 			if (this.toolList.length === 0) {
 				Ember.$("#list-title").css("border-style", "solid");
 				Ember.$("#list-title").css("border-color", "#e30000");
 			}
-			
+
 			if (Ember.$("#user-to-transfer-to").val() !== null && this.toolList.length !== 0) {
 				   var userid = parseInt(Ember.$("#user-to-transfer-to").val());
                    let _this = this;
@@ -80,42 +80,42 @@ export default Ember.Controller.extend({
 					this.set('toolList', []);
 			}
         },
-		
+
         updateSearch(target) {
 			Ember.$(".search-box").val('');
             let params = this.queryParams.get('0');
             params.currentUser = this.get('session').get('data.currentUserID');
-            
+
             if (target.getAttribute('name') === "status") {
                 params.status = target.value;
-                
+
             } else if (target.getAttribute('name') === "brand") {
                 params.brand = target.value;
-                
+
             } else if (target.getAttribute('name') === "type") {
                 params.type = target.value;
-                
+
             } else if (target.getAttribute('name') === "owner") {
                 params.userID = target.value;
             }
-            
+
             var set = this.set.bind(this, 'model.tools');
             Ember.$.getJSON(`${config.APP.api_url}${config.APP.api_namespace}/search`, params).then(set);
         },
-        
+
         fuzzySearch(value) {
-			
+			    Ember.$(".search-parameter").val('');
             var set = this.set.bind(this, 'model.tools');
             let currentUser = this.get('session').get('data.currentUserID');
-            
+
             if( value !== "" ) {
-				Ember.$(".search-parameter").val('');
+				        Ember.$(".search-parameter").val('');
                 Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search', { currentUser: currentUser, parameter: value } ).then(set);
             } else {
                 Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search?currentUser=' + currentUser + '&status=&userID=&type=&brand=').then(set);
             }
         }
-		
+
 
     }
 

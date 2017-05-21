9a7c7f179b6bf83bdb5ba34a183367af1ebf644a
Amos Endersen <amenders@iupui.edu>
2017-05-21 10:58:27 -0400

    reactivated API calls

--- app/routes/info.js
+++ app/routes/info.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import config from '../config/environment';
 
-export default Ember.Route.extend(/*AuthenticatedRouteMixin, */{
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model(params) {
 		return Ember.RSVP.hash({
             tool: this.get('store').findRecord('tool', params.id),
--- app/routes/scan.js
+++ app/routes/scan.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from '../config/environment';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(/*AuthenticatedRouteMixin, */{
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
     session: Ember.inject.service('session'),
 
 	model() {

1e1f4e107083d18a9fe312e51661c42514baea92
Josiah Campbell <jbcampbe@iupui.edu>
2017-05-28 21:55:49 -0400

    switching file to soft tabs

--- app/routes/scan.js
+++ app/routes/scan.js
@@ -5,13 +5,13 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     session: Ember.inject.service('session'),
 
-	model() {
-		let _tools = null;
-		if (this.controller != null) {
-			_tools = Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search', this.controller.get('_query'));
-		} else {
-			_tools = Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search?currentUser=' + this.get('session').get('data.currentUserID') + '&status=&userID=&type=&brand=');
-		}
+    model() {
+        let _tools = null;
+        if (this.controller != null) {
+            _tools = Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search', this.controller.get('_query'));
+        } else {
+            _tools = Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/search?currentUser=' + this.get('session').get('data.currentUserID') + '&status=&userID=&type=&brand=');
+        }
 
         let _users = null;
         if (this.get('session').get('data.currentUserRole') === 'Administrator') {
@@ -20,12 +20,12 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
             _users = Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/users?currentUser=' + this.get('session').get('data.currentUserID'));
         }
 
-		return Ember.RSVP.hash({
+        return Ember.RSVP.hash({
             tools: _tools,
-			status: Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/status'),
-			selectUsers: _users,
-			types: Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/types'),
+            status: Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/status'),
+            selectUsers: _users,
+            types: Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/types'),
             brands: Ember.$.getJSON(config.APP.api_url + config.APP.api_namespace + '/brands')
-		});
-	}
+        });
+    }
 });

e1c174f1fb0a27f4a4046e2baa50692bf041305b
Josiah Campbell <jbcampbe@iupui.edu>
2017-06-18 22:02:37 -0400

    fixing indentaiton in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-  globals: {
-    server: true,
-  },
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
-  },
-  extends: 'eslint:recommended',
-  env: {
-    browser: true
-  },
-  rules: {
-  }
+    globals: {
+        server: true,
+    },
+
+    root: true,
+
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module'
+    },
+
+    extends: 'eslint:recommended',
+
+    env: {
+        browser: true
+    },
+
+    rules: {
+
+    }
 };

c4fd5f6b91fe1e74236bfc494aeb63c83e0c4116
Josiah Campbell <jbcampbe@iupui.edu>
2017-07-05 23:39:25 -0400

    writing/fixing tests

--- app/components/tool-search-entry/component.js
+++ app/components/tool-search-entry/component.js
@@ -9,5 +9,7 @@ export default Ember.Component.extend({
         }
 
         return 'fa-chevron-right';
-    })
+    }),
+
+    onResultSelected: () => {}
 });
--- app/controllers/search.js
+++ app/controllers/search.js
@@ -3,6 +3,6 @@ import SearchMixin from '../mixins/search-driver';
 
 export default Ember.Controller.extend(SearchMixin, {
     goToInfoPage(toolId) {
-        this.transitionToRoute('info', toolId)
+        this.transitionToRoute('info', toolId);
     }
 });

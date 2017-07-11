2f630a9fe8e540b15a5779db93add20391dc205a
Josiah Campbell <jbcampbe@iupui.edu>
2017-07-11 18:34:08 -0400

    making ember build correctly do prod build on master branch

--- scripts/prep-for-deploy.sh
+++ scripts/prep-for-deploy.sh
@@ -1,3 +1,16 @@
+#!/bin/sh
+
+branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+echo $branch
+if [ $branch = "master" ]
+then
+    echo "ember build --prod"
+    ember build --prod
+else
+    echo "ember build"
+    ember build
+fi
+
 find . -maxdepth 1 ! -iname dist ! -iname ".." ! -iname "." ! -iname ".git" -exec rm -rf {} \;
 mv dist/* .
 rm -rf dist

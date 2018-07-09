8f3c122f6c39bb5b0f9c812a3813532a6286465f
Josiah Campbell <josiahc495@gmail.com>
2018-07-09 18:24:22 -0400

    init parcel project

--- app/controllers/search.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import SearchMixin from '../mixins/search-driver';
-
-export default Ember.Controller.extend(SearchMixin, {
-    goToInfoPage(toolId) {
-        this.transitionToRoute('info', toolId);
-    }
-});
--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-    redirect() {
-        this.transitionTo('scan');
-    }
-});

6068185c2c520cfb015eef7e136379f8c2f87906
Josiah Campbell <josiahc495@gmail.com>
2018-07-11 19:21:54 -0400

    tests passing

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,13 @@ module.exports = {
     browser: true,
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
-  // required to lint *.vue files
   plugins: [
     'vue'
   ],
-  // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',

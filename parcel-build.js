877a99efd6dea3814209216e462cf3902e8057c1
Josiah Campbell <josiahc495@gmail.com>
2018-07-27 01:12:58 -0400

    add cordova to project and setup build

---
+++ parcel-build.js
@@ -0,0 +1,26 @@
+const Bundler = require('parcel-bundler')
+const Path = require('path')
+
+// Entrypoint file location
+const file = Path.join(__dirname, './index.html')
+
+// Bundler options
+const options = {
+  outDir: './cordova/www',
+  outFile: 'index.html',
+  publicUrl: './',
+  watch: false,
+  cache: true,
+  cacheDir: '.cache',
+  contentHash: false,
+  minify: true,
+  target: 'browser',
+  https: false,
+  logLevel: 1,
+  hmrPort: 0,
+  sourceMaps: false,
+  hmrHostname: '',
+  detailedReport: false
+}
+
+new Bundler(file, options).bundle()
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import attachFastClick from 'fastclick'
 
 Vue.config.productionTip = false
+
+attachFastClick(document.body)
 
 /* eslint-disable no-new */
 new Vue({

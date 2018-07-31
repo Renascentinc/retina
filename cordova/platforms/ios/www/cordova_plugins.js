f2b1fb31407b2861c778185abcd7929cc97299b0
Josiah Campbell <josiahc495@gmail.com>
2018-07-30 20:59:08 -0400

    trying to fix cordova setup to be as easy to use as possible

---
+++ cordova/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "phonegap-nfc.NFC",
+    "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+    "pluginId": "phonegap-nfc",
+    "runs": true
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "phonegap-nfc": "1.0.3"
+};
+// BOTTOM OF METADATA
+});

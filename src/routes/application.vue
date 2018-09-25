2dcafc18a36dd979f958f474fa411bcc038d2cfe
Josiah Campbell <josiahc495@gmail.com>
2018-09-25 16:46:32 -0400

    adding auth and protecting routes

--- src/routes/application.vue
+++ src/routes/application.vue
@@ -1,56 +1,68 @@
 <template>
   <div class="main-application">
     <vue-drawer-layout
-          ref="drawer"
-          :drawer-width="200"
-          :enable="false"
-          :animatable="true"
-          :backdrop="true"
-          @mask-click="closeDrawer">
+      ref="drawer"
+      :drawer-width="200"
+      :enable="false"
+      :animatable="true"
+      :backdrop="true"
+      @mask-click="closeDrawer">
 
-          <div class="drawer-content" slot="drawer">
-            <avatar username="Josiah Campbell"></avatar>
+      <div
+        slot="drawer"
+        class="drawer-content">
+        <avatar username="Josiah Campbell"/>
+      </div>
+
+      <div
+        slot="content"
+        class="main-content">
+        <router-view />
+
+        <div class="nav-bar">
+          <div class="icon-text-container">
+            <button
+              class="material-icons menu-icon"
+              @click="openDrawer">menu</button>
+            <span class="icon-subtext">MENU</span>
           </div>
 
-          <div class="main-content" slot="content">
-              <router-view />
-
-              <div class="nav-bar">
-                <div class="icon-text-container">
-                  <button class="material-icons menu-icon" @click="openDrawer">menu</button>
-                  <span class="icon-subtext">MENU</span>
-                </div>
-
-                <div class="icon-text-container">
-                  <router-link class="fas fa-toolbox" to="/tools"></router-link>
-                  <span class="icon-subtext">TOOLS</span>
-                </div>
-
-                <div class="icon-text-container">
-                  <router-link class="material-icons add-icon" :to="{ name: 'newTool' }">add</router-link>
-                  <span class="icon-subtext">ADD</span>
-                </div>
-              </div>
+          <div class="icon-text-container">
+            <router-link
+              class="fas fa-toolbox"
+              to="/tools"/>
+            <span class="icon-subtext">TOOLS</span>
           </div>
+
+          <div class="icon-text-container">
+            <router-link
+              :to="{ name: 'newTool' }"
+              class="material-icons add-icon">add</router-link>
+            <span class="icon-subtext">ADD</span>
+          </div>
+        </div>
+      </div>
     </vue-drawer-layout>
   </div>
 </template>
 
 <script>
 import Avatar from 'vue-avatar'
+import authenticatedRouteMixin from '../mixins/authenticatedRoute'
 
 export default {
-  name: 'application',
+  name: 'Application',
   components: {
     Avatar
   },
+  mixins: [authenticatedRouteMixin],
   methods: {
-    closeDrawer() {
-      this.$refs.drawer.toggle(false);
+    closeDrawer () {
+      this.$refs.drawer.toggle(false)
     },
 
-    openDrawer() {
-      this.$refs.drawer.toggle(true);
+    openDrawer () {
+      this.$refs.drawer.toggle(true)
     }
   }
 }
--- src/routes/login.vue
+++ src/routes/login.vue
@@ -4,36 +4,59 @@
     <img class="logo" src="../assets/icons/web/white_transparent_512x512.png"></img>
   </div>
   <div class="login-input-container">
-    <input class="login-input" placeholder="username"/>
-    <input class="login-input" placeholder="password"/>
-    <button class="login-btn">LOGIN</button>
+    <input class="login-input" placeholder="username" type="email" :value="email"/>
+    <input class="login-input" placeholder="password" type="password" :value="password"/>
+    <input class="login-input" placeholder="organization name" :value="organizationName"/>
+    <button class="login-btn" @click="attemptUserLogin">LOGIN</button>
   </div>
 </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
   name: 'login',
 
-  apollo: {
-    mutation: gql`mutation attemptUserLogin($organization_name: String!, $email: String!, $password: String!) {
-       login(organization_name: $organization_name, email: $email, password: $password) {
-        token,
-        user {
-    	  id,
-          first_name,
-          last_name,
-          email,
-          phone_number,
-          role,
-          status
+  data() {
+    return {
+      organizationName: 'DeWalt',
+      email: 'bob.thompson@somecompany.com',
+      password: 'Test1234!'
+    }
+  },
+
+  methods: {
+    attemptUserLogin() {
+      this.$apollo.mutate({
+        mutation: gql`mutation attemptUserLogin($organization_name: String!, $email: String!, $password: String!) {
+           login(organization_name: $organization_name, email: $email, password: $password) {
+            token,
+            user {
+        	    id,
+              first_name,
+              last_name,
+              email,
+              phone_number,
+              role,
+              status
+            }
+          }
+        }`,
+        variables: {
+          organization_name: this.organizationName,
+          email: this.email,
+          password: this.password
         }
-      }
-    }`,
-    variables: {
-      organization_name: this.organizationName,
-      email: this.email,
-      password: this.password
+      }).then(result => {
+        let { data: { login: { token, user } } } = result
+
+        window.localStorage.setItem('token', token)
+        window.localStorage.setItem('currentUser', user)
+        this.$router.push({ path: '/' })
+      }).catch(error => {
+        console.error(error)
+      })
     }
   }
 }

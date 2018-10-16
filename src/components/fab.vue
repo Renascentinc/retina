184a658fc4e714dc78f66539c2452962cbc2a457
Amos Endersen <amenders@iupui.edu>
2018-10-15 21:04:51 -0400

    added .vscode/ to .gitignore

---
+++ src/components/fab.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    class="fab"
+    @click="onClick">
+    <div class="fab-icon-container">
+      <i
+        :class="iconClass"
+        class="fas"/>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Fab',
+
+  props: {
+    iconClass: {
+      type: String,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../styles/variables';
+
+  .fab {
+      border: none;
+      background-color: #CE352F;
+      border-radius: 50%;
+      color: white;
+      height: 57px;
+      width: 57px;
+      font-size: 24px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+  }
+</style>

c1988e24157a779bc148c92a9742392e3020ba71
Josiah Campbell <josiahc495@gmail.com>
2018-10-21 19:05:20 -0400

    fix router

--- src/router.js
+++ src/router.js
@@ -5,23 +5,17 @@ import Application from './routes/application'
 import Tools from './routes/tools'
 
 // lazy loaded routes
-// const Configuration = () => import('./routes/configuration')
-// const Reports = () => import('./routes/reports')
-// const UserDetail = () => import('./routes/user-detail')
-// const Users = () => import('./routes/users')
-// const NewTool = () => import('./routes/new-tool')
-// const ToolDetail = () => import('./routes/tool-detail')
-
-import Configuration from './routes/configuration'
-import Reports from './routes/reports'
-import UserDetail from './routes/user-detail'
-import Users from './routes/users'
-import NewTool from './routes/new-tool'
-import ToolDetail from './routes/tool-detail'
+const Configuration = () => import('./routes/configuration')
+const Reports = () => import('./routes/reports')
+const UserDetail = () => import('./routes/user-detail')
+const Users = () => import('./routes/users')
+const NewTool = () => import('./routes/new-tool')
+const ToolDetail = () => import('./routes/tool-detail')
 
 Vue.use(Router)
 
 export default new Router({
+  // TODO: would like to re-enable this but causing issues with the deploy on S3
   // mode: 'history',
   routes: [
     {

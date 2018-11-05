86922d2bfb5b5f2f62d99c6d4abddda6c4e8e171
Josiah Campbell <josiahc495@gmail.com>
2018-11-04 21:29:23 -0500

    fix add tools page issues

--- src/routes/new-tool.vue
+++ src/routes/new-tool.vue
@@ -365,7 +365,7 @@ export default {
       price: null,
       photo: null,
       status: statuses[1],
-      currentState: 2,
+      currentState: 1,
       purchaseDate: null,
       getAllConfigurableItem: [],
       getAllUser: [],
